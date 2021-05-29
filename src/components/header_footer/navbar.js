c95e2781b1e014f2e04bd086bbb1c191a34e50ec
DanielLaborda <dani.laborda90@gmail.com>
2021-05-29 19:27:43 +0200

    logo navbar

--- src/components/header_footer/navbar.js
+++ src/components/header_footer/navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../../static/images/logo.png';
 
 const Navbar = (props) => {
     return(
         <div>
             <div className="logo-side">
             <NavLink exact to="/">
-                <img src={Logo} />
+                <img src='../../../static/images/logo.png' />
             </NavLink>
             </div>
             <div className="left-side">
