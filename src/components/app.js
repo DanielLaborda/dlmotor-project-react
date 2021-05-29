a724fb8b3443bccf273b797bbfeec50063dcf64a
DanielLaborda <dani.laborda90@gmail.com>
2021-05-29 03:52:56 +0200

    home page

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Switch, Route } from  'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
 import history from "../history";
 import Layout from './layout';
 import Home from './pages/home';
