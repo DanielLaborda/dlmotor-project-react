83be4599a541d8f134e33fe36ef1d4bebe50593a
DanielLaborda <dani.laborda90@gmail.com>
2021-05-29 16:29:58 +0200

    testing up heroku

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -24,49 +24,9 @@ const Navbar = (props) => {
                         </div>
                     </div>
                 </div>
-
-
-                {/* <ul className={click ? "navbar__options active" : "navbar__options"}>
-                    <li className='navbar__options__option' onClick={closeMobileMenu}>
-                        <a onClick={() => history.push('/categories')}>
-                            <div className='background-yellow' />
-                            <div className='background-blue' />
-                            <div className='text' >Modelos</div>
-                        </a>
-                    </li>
-                    <li className='navbar__options__option' onClick={closeMobileMenu}>
-                        
-                        <a onClick={() => history.push('/garage')}>
-                            <div className='background-yellow' />
-                            <div className='background-blue' />
-                            <div className='text' >Garage</div>
-                        </a>
-                    </li>
-                    <li className='navbar__options__option' onClick={closeMobileMenu}>
-                        <a onClick={() => history.push('/racing-team')}>
-                            <div className='background-yellow' />
-                            <div className='background-blue' />
-                            <div className='text' >Racing team</div>
-                        </a>
-                    </li>                   
-                </ul> */}
-                {/* <a onClick={() => history.push('/users')}>user<i className="far fa-user"/></a> */}
-                    
-                {/* <div className="mobile-menu" onClick={handleClick}>
-                    {click ? (
-                    <div className="menu-icon">
-                        <i className="fas fa-bars"/> 
-                    </div>
-                    ) : (
-                    <div className="menu-icon">
-                        <i className="fas fa-bars"/> 
-                    </div>
-                    )}
-                </div>
-               */}
             </div>
         );
     
 }
 
-export default Navbar;
+export default withRouter(Navbar);
